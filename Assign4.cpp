9f9c1b63f5f86b1fddebfe3efbc131ec336416cf
Kevin Miyata <kjm015@yahoo.com>
2019-03-02 16:12:49 -0600

    add logging to main

--- Assign4.cpp
+++ Assign4.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 int main() {
+    cerr << "Hello, world!" << endl;
+
     readFile();
+
+    cerr << "I read the file!" << endl;
 
     int activeProcessCount = 0;
     int idleTimer = 0;
 
     while (condition(timer)) {
 
+        cerr << "Starting the process loop!" << endl;
+
         // Pull processes from entry queue to ready queue and print result
         while (activeProcessCount < AT_ONCE and !entryQueue.empty()) {
             Process *movedProcess = entryQueue.front();
             if (movedProcess->getArrivalTime() > timer) {
-                continue;
+                break;
             }
             entryQueue.pop();
             cerr << "Process " << movedProcess->getProcessName() << " moved to the ready queue @ time" << timer << endl;

592c13ca541009aa4bff5b3e2dc2a2163a465523
Kevin Miyata <kjm015@yahoo.com>
2019-03-04 17:40:26 -0600

    nevermind

--- Assign4.h
+++ Assign4.h
@@ -108,13 +108,13 @@ void setProcess(Process *process, const char *line) {
 
     strcpy(buffer, line);
 
-    processName = strtok(buffer, "\t");
+    processName = strtok(buffer, " ");
     process->setProcessName(processName);
 
-    data = strtok(nullptr, "\t");
+    data = strtok(nullptr, " ");
     process->setPriority(static_cast<unsigned int>(atoi(data)));
 
-    data = strtok(nullptr, "\t");
+    data = strtok(nullptr, " ");
     process->setArrivalTime(static_cast<unsigned int>(atoi(data)));
 }
 
